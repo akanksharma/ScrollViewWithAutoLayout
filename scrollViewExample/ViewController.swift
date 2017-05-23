0bf7bf43522b1724b0184fa255178fd4783d5973
Akanksha Sharma <akanksha.sharma@nexgear.co>
2017-05-23 15:26:13 +0530

    Scroll view Auto Layout

---
+++ scrollViewExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  scrollViewExample
+//
+//  Created by Akanksha Sharma on 23/05/17.
+//  Copyright © 2017 Nexgear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //scrollView.contentSize = CGSize.init(width: 375, height: 600)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
